4e5cee9d6e7e2868b94cd88033e03ace1ee5caf8
Barthélemy Brodzinski <SemolinaWatvr@gmail.com>
2025-10-19 16:55:21 +0200

    leFameuxBob

--- modules/rebond.js
+++ modules/rebond.js
@@ -1,9 +1,17 @@
 function hello () {
+    
+
+
+
+
+
     let bob = document.createElement('p');
     document.body.append(bob);
 
     bob.id = "leFameuxBob";
     bob.textContent = "SALUT?";
+
+    bob.addEventListener("click", console.log("coucou"))
 }
 
 export default hello;
